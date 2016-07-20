ccd0f5b45c39cba801c63befc1a1d958ecc0e51b
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-07-20 14:17:37 -0700

    draggable and mytextcolor

--- client/js/directives/myDirective.js
+++ client/js/directives/myDirective.js
@@ -2,7 +2,57 @@
   angular
     .module('myApp')
       .directive('myTable', myTable)
+      .directive('myTextColor', myTextColor)
+      .directive('myDraggable', myDraggable)
       .controller('MyTableController', MyTableController);
+
+  function myDraggable($document) {
+    return {
+      link: function(scope, element, attr) {
+        var startX = 0, startY = 0, x = 0, y = 0;
+
+        element.css({
+          position: 'relative',
+          // border: '1px solid red',
+          // backgroundColor: 'lightgrey',
+          cursor: 'pointer',
+        });
+
+        element.on('mousedown', function(event) {
+          // Prevent default dragging of selected content
+          event.preventDefault();
+          startX = event.pageX - x;
+          startY = event.pageY - y;
+          $document.on('mousemove', mousemove);
+          $document.on('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+          y = event.pageY - startY;
+          x = event.pageX - startX;
+          element.css({
+            top: y + 'px',
+            left:  x + 'px',
+          });
+        }
+
+        function mouseup() {
+          $document.off('mousemove', mousemove);
+          $document.off('mouseup', mouseup);
+        }
+      },
+    };
+  }
+
+  function myTextColor() {
+    const directive = {
+      restrict: 'A',
+      link: (scope, elem, attrs) => {
+        elem.css('color', attrs.myTextColor);
+      },
+    };
+    return directive;
+  }
 
   function myTable() {
     return {
@@ -13,6 +63,9 @@
       templateUrl: './html/myTable.directive.html',
       scope: {
         list: '=',
+      },
+      link: (scope, element) => {
+        element.children('table.table').addClass('table-hover');
       },
     };
   }
